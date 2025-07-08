30f896b06a8c06adcc6367d4cfc9d0a70b74e317
hari-343 <harihk0707@gmail.com>
2025-07-08 12:10:20 +0530

    First language app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+import TabItem from './components/TabItem'
+import './App.css'
 
-function App() {
+const languageGreetingsList = [
+  {
+    id: 'bfdf40eb-eec9-4a66-a493-752fe689f0d0',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/multilingual_greeting/english-greetings-img.png',
+    buttonText: 'English',
+    imageAltText: 'english',
+  },
+  {
+    id: '0ceda891-2a0c-49e2-8c62-68e78180bac6',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/multilingual_greeting/tamil-greetings-img.png',
+    buttonText: 'Tamil',
+    imageAltText: 'tamil',
+  },
+  {
+    id: '89537778-7a46-4c58-988c-0adc931d087c',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/multilingual_greeting/telugu-greetings-img.png',
+    buttonText: 'Telugu',
+    imageAltText: 'telugu',
+  },
+]
+
+// Replace your code here
+const App = () => {
+  const [idElement, setIdElement] = useState(languageGreetingsList[0].id)
+  const changeState = id => {
+    setIdElement(id)
+  }
+  const getGreeting = () => {
+    const result = languageGreetingsList.find(i => i.id === idElement)
+    return result
+  }
+  const {imageUrl, imageAltText} = getGreeting()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="heading">Multilingual Greetings</h1>
+      <ul className="tabs-list">
+        {languageGreetingsList.map(each => (
+          <TabItem
+            details={each}
+            key={each.id}
+            isActive={idElement === each.id}
+            changeState={changeState}
+          />
+        ))}
+      </ul>
+      <img src={imageUrl} alt={imageAltText} className="img" />
     </div>
-  );
+  )
 }
-
-export default App;
+export default App
